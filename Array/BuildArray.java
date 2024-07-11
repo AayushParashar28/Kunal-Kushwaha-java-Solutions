2caeafbf260d1faf8a10acc3fc3a2ebd70c3ccf7
AayushParashar28 <ayushparashar6203@gmail.com>
2024-07-11 11:41:26 +0530

    Arrays

--- Array/BuildArray.java
+++ Array/BuildArray.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BuildArray {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
-        
+        System.err.print("Enter Array Size : ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n ; i++){
+            arr[i] = s.nextInt();
+        }
+
+        System.err.println(Arrays.toString(arr));
     }
 }
