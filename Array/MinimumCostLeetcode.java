6ca2125280b2a93bb9b953755e50324b5db7f85b
AayushParashar28 <ayushparashar6203@gmail.com>
2024-10-31 00:27:25 +0530

    Arrays

--- Array/MinimumCostLeetcode.java
+++ Array/MinimumCostLeetcode.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class MinimumCostLeetcode {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter Array length : ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter Array " + i + "st element :");
+            arr[i] = s.nextInt();
+        }
     }
 }
