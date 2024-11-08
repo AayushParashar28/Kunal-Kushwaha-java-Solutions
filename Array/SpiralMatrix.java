38f571f186fbe90dc8e471ce7082b3da559b5a93
AayushParashar28 <ayushparashar6203@gmail.com>
2024-11-08 22:32:06 +0530

    Arrays

--- Array/SpiralMatrix.java
+++ Array/SpiralMatrix.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class SpiralMatrix {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
     }
 
     public List<Integer> spiralOrder(int[][] matrix) {
