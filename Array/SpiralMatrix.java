87fef0d32320c63deada4ff2a90f4dbf025a7861
AayushParashar28 <ayushparashar6203@gmail.com>
2024-11-10 18:04:39 +0530

    Arrays

--- Array/SpiralMatrix.java
+++ Array/SpiralMatrix.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 public class SpiralMatrix {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter Array row :");
+        System.out.print("Enter ARRAY ROW: ");
         int n = s.nextInt();
+        System.out.print("Enter Array Column: ");
+        int m = s.nextInt();
+
+        int[][] arr = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print("Enter " + i + "th element of Array: ");
+                arr[i][j] = s.nextInt();
+            }
+        }
+        System.out.println("Original Array:");
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
     }
 
     public List<Integer> spiralOrder(int[][] matrix) {
