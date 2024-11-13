9eb0c6191b4cae004abb47014d6c3d2baa44acbb
AayushParashar28 <ayushparashar6203@gmail.com>
2024-11-13 18:58:30 +0530

    Arrays

--- Array/SpiralMatrix2.java
+++ Array/SpiralMatrix2.java
@@ -2,4 +2,9 @@ public class SpiralMatrix2 {
     public static void main(String[] args) {
         
     }
+
+   static int[][] generateMatrix(int n) {
+    int[][] matrix = new int[n][n];
+        return matrix;
+    }
 }
