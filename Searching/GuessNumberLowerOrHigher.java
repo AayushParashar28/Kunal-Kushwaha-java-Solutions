931cbcaa225bd18e2779fe781043e56a114938d1
AayushParashar28 <ayushparashar6203@gmail.com>
2024-11-25 22:13:10 +0530

    Searching

--- Searching/GuessNumberLowerOrHigher.java
+++ Searching/GuessNumberLowerOrHigher.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class GuessNumberLowerOrHigher{
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = s.nextInt();
+    }
 }
