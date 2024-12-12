705d23f25bc5ec4cfc60f8e2720f3de27fd22c66
AayushParashar28 <ayushparashar6203@gmail.com>
2024-12-12 19:53:37 +0530

    Searching

--- Searching/PeakIndexInMountainArray.java
+++ Searching/PeakIndexInMountainArray.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class SearchInsertPosition {
+public class PeakIndexInMountainArray {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter Array Size :");
@@ -15,29 +15,25 @@ public class SearchInsertPosition {
 
         System.out.println(Arrays.toString(arr));
 
-        System.out.print("Enter Target Number :");
-        int target = s.nextInt();
-
-        int ans = searchPosition(arr, target);
-        System.out.println("Target Number Position : " + ans);
+        int ans = peakIndexInMountainArray(arr);
+        System.out.println("Peak Index is : " + ans);
     }
 
-    static int searchPosition(int[] arr, int target) {
+    static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-
+        int temp = 0;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
 
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                temp = mid;
+                end = mid - 1;
+            }
         }
 
-        return start;
+        return temp;
     }
 }
