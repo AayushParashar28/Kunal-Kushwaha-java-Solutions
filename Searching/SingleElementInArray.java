461f8471d109a50b64660e626fb33046cd21b7d1
AayushParashar28 <ayushparashar6203@gmail.com>
2024-12-19 14:35:03 +0530

    Searching

--- Searching/SingleElementInArray.java
+++ Searching/SingleElementInArray.java
@@ -11,5 +11,26 @@ public class SingleElementInArray{
             System.out.print("Enter element at " + i + "index : ");
             arr[i] = s.nextInt();
         }
+
+        int ans = singleNonDuplicate(arr);
+        System.out.println("Single Element in array is : " + ans);
+    }
+
+    public static int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int end = n/2;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+            
+            if(nums[2*mid] != nums[2*mid + 1]) {
+                end = mid;
+            }
+
+            else start = mid + 1;
+            
+        }
+        return nums[2*start];
     }
 }
