793ee6fa7f3902056b67c9f322ce95534f3d47ac
AayushParashar28 <ayushparashar6203@gmail.com>
2025-02-04 23:17:54 +0530

    Sorting

--- Sorting/CycleSort.java
+++ Sorting/CycleSort.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class CycleSort {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0 ; i < n ; i++) {
+            System.out.print("Enter Array Element : ");
+            arr[i] = s.nextInt();
+        }
+
+        
     }
 }
