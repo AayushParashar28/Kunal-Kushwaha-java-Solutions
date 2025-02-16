24d866920d67ced8a912347eb2435a22994ba088
AayushParashar28 <ayushparashar6203@gmail.com>
2025-02-16 22:37:35 +0530

    String

--- String/string.java
+++ String/string.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class string {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = s.nextLine();
+        System.out.println("Enter a character: ");
+        char ch = s.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+                }
+                }
+                System.out.println("The character '" + ch + "' appears " + count + " times in the string.");
     }
 }
