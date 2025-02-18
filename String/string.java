f4ea5e26152787bd0d3a719b1918aeee323e8efe
AayushParashar28 <ayushparashar6203@gmail.com>
2025-02-19 00:12:15 +0530

    String

--- String/string.java
+++ String/string.java
@@ -5,7 +5,12 @@ public class string {
 
         System.out.println(a==b);
 
-        System.out.println(a.equals(b));
-         
+        System.out.println(a.equals(b)); 
+        
+        
+        String name1 = new String("Kunal");
+        String name2 = new String("kunal");
+
+        System.out.println(name1.equals(name2)); 
     }
 }
---
+++ String/string1.java
@@ -0,0 +1,15 @@
+public class string1 {
+    public static void main(String[] args) {
+        //printstreaming
+        float a = 453.1234f;
+        System.out.printf("The value of a is: %.2f%n", a);
+
+        System.out.printf("Pie : %.3f%n", Math.PI );
+
+
+        System.out.println('a' + 'b');
+        System.out.println("a" + " b");
+        System.out.println('a' + 100);
+        System.out.println("A" + 100);
+    }
+}
