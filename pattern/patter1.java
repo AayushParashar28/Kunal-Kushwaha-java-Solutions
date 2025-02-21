4c165b11b2cdff25d096ccc127be24ea974a467c
AayushParashar28 <ayushparashar6203@gmail.com>
2025-02-21 23:02:59 +0530

    Pattern

--- pattern/patter1.java
+++ pattern/patter1.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class patter1 {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
