077f8450b588c31707cf1f643a54d3ab40e9b918
AayushParashar28 <ayushparashar6203@gmail.com>
2025-02-28 23:04:25 +0530

    pattern

--- pattern/pattern25.java
+++ pattern/pattern25.java
@@ -4,8 +4,38 @@ public class pattern25 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter Number : ");
-        int n = s.nextInt();
-
         
+        int i, j;
+        int n = s.nextInt();
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < (2 * n); j++)
+            {
+                if(i + j <= n - 1)  // upper left triangle
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+                if((i + n) <= j) // upper right triangle
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < (2 * n); j++)
+            {
+                if(i >= j)  // bottom left triangle
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+                if(i >= (2 * n - 1) - j)  // bottom right triangle
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ pattern/pattern26.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pattern26 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enterr Number : ");
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part (mirroring the upper part)
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
