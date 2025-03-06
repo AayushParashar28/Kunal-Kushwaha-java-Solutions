e9fdbeabcf1e12ebcbae0f01de4bd01c9f35506f
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-06 23:21:17 +0530

    Pattern

--- pattern/pattern28.java
+++ pattern/pattern28.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class pattern28 {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enterr Number : ");
+        int n = s.nextInt();
+
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
