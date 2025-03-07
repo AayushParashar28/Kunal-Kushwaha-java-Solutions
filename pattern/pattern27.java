33be17163b69e679822c7da7d7d9da6d183b0142
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-07 23:03:14 +0530

    pattern

--- pattern/pattern27.java
+++ pattern/pattern27.java
@@ -5,5 +5,26 @@ public class pattern27 {
         Scanner s = new Scanner(System.in);
         System.out.print("Enterr Number : ");
         int n = s.nextInt();
+
+        int start = 1;
+        int end = n * (n + 1);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            int rowCount = n - i;
+            int tempStart = start;
+            int tempEnd = end - rowCount + 1;
+            for (int j = 0; j < rowCount; j++) {
+                System.out.print(tempStart++ + " ");
+            }
+            for (int j = 0; j < rowCount; j++) {
+                System.out.print(tempEnd++ + " ");
+            }
+            start += rowCount;
+            end -= rowCount;
+            System.out.println();
+        }
   }
 }
