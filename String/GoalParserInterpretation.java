e16e36e291e06cb6a1e80b3247239496b6e31c5a
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-12 23:21:19 +0530

    String

--- String/GoalParserInterpretation.java
+++ String/GoalParserInterpretation.java
@@ -1,5 +1,13 @@
 public class GoalParserInterpretation {
     public static void main(String[] args) {
-        
+        String str = "G()(al)";
+        System.out.println(interpret(str)); // Output: Goal
+        String ab = "G()()()()(al)";
+        System.out.println(interpret(ab)); // Output: Gooooal
+    }
+
+    public static String interpret(String command) {
+        String str = command.replace("()" , "o").replace("(al)" , "al") ; 
+        return str ;
     }
 }
