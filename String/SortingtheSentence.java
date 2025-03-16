5b75994e727e18838ff20bed65476982be5da737
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-16 20:06:07 +0530

    String

--- String/SortingtheSentence.java
+++ String/SortingtheSentence.java
@@ -1,5 +1,18 @@
 public class SortingtheSentence {
     public static void main(String[] args) {
+        String s = "is2 sentence4 This1 a3";
+
+        System.out.println(sortSentence(s));
+    }
+
+    public static String sortSentence(String s) {
+        String[] words = s.split(" ");
+		String[] cache = new String[words.length];
+		for (String word : words) {
+			int index = word.charAt(word.length() - 1) - '1';
+			cache[index] = word.substring(0, word.length() - 1);
+		}
+		return String.join(" ", cache);
         
     }
 }
