24f536f524b707801d037db6cc2fcfc8f242f93b
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-19 14:49:24 +0530

    String

--- String/ToLower.java
+++ String/ToLower.java
@@ -1,5 +1,14 @@
 public class ToLower {
     public static void main(String[] args) {
-        
+       String s = "Hello";
+       String t = "here";
+       String u = "LOVELY";
+       System.out.println(toLowerCase(s));
+       System.out.println(toLowerCase(t));
+       System.out.println(toLowerCase(u));
     }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+   }
 }
