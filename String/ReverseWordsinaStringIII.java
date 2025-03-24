cc91b6dd7b8121a6897ece6e6a8990bd7f9e5a9a
AayushParashar28 <ayushparashar6203@gmail.com>
2025-03-24 20:08:58 +0530

    String

--- String/ReverseWordsinaStringIII.java
+++ String/ReverseWordsinaStringIII.java
@@ -1,5 +1,32 @@
 public class ReverseWordsinaStringIII {
     public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+    public static String reverseWords(String s) {
+        final int len = s.length(); 
         
+        if(len == 1) 
+            return s;
+        
+        int firstIndex, lastIndex;
+        char[] ch = s.toCharArray(); 
+        char temp;
+        
+        for(int index = 0 ; index < len ; index++){
+            
+            firstIndex = index;
+            
+            while(++index < len && ch[index] != ' '); 
+            
+            lastIndex = index - 1; 
+            while(firstIndex < lastIndex){
+                temp = ch[firstIndex];
+                ch[firstIndex++] = ch[lastIndex];
+                ch[lastIndex--] = temp;
+            }
+        }
+        
+        return new String(ch);
     }
 }
