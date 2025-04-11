d4f193cd1d060fa879d5d6a8b4f4848c6654e0e7
Aayush Parashar <ayushparashar6203@gmail.com>
2025-04-11 23:21:21 +0530

    String

--- String/ReversePrefixofWord.java
+++ String/ReversePrefixofWord.java
@@ -1,5 +1,16 @@
 public class ReversePrefixofWord {
     public static void main(String[] args) {
-        
+        String word = "abcdefd";
+        char ch = 'd';
+        System.out.println(reversePrefix(word,ch));
     }
+    public static String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch); 
+           return index >= 0 ? 
+                 new StringBuilder(word.substring(0,index+1))
+                 .reverse()
+                 .append(word.substring(index+1,word.length()))
+                 .toString() : 
+                 word; 
+     }
 }
